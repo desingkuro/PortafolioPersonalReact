9f84b26d3aa97c6a1b7ba740a0c3b890b4921a21
desingkuro <andresmauricio745@gmail.com>
2023-12-02 11:10:08 -0500

    nuevo titulo seccion proyectos

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -8,14 +8,18 @@ export function Proyectos(){
     const {proyectos} = useContext(contextoPrincipal);
 
     return(
-        <section className='contenedorProyectos'>
-            {
-                proyectos.map((e,i)=>{
-                    return(
-                        <CardProyectos tecnologia={e.tecnologias} titulo={e.titulo} imagen={e.img} key={i} git={e.git} proyectoLink={e.link}/>
-                    )
-                })
-            }
-        </section>
+        
+        <>
+            <h2 className='tituloSectionProyecto'>Proyectos realizados</h2>
+            <section className='contenedorProyectos'>
+                {
+                    proyectos.map((e,i)=>{
+                        return(
+                            <CardProyectos tecnologia={e.tecnologias} titulo={e.titulo} imagen={e.img} key={i} git={e.git} proyectoLink={e.link}/>
+                        )
+                    })
+                }
+            </section>
+        </>
     )
 }
