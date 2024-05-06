e9183bbede1f28e91019be5a784b454ec1e2af70
desingkuro <andresmauricio745@gmail.com>
2024-05-06 11:02:17 -0500

    añadido de cursos

--- context/contexto.jsx
+++ context/contexto.jsx
@@ -4,17 +4,22 @@ import tesla from'../src/assets/tesla.png'
 import encriptador from'../src/assets/encriptador.jpg'
 import barbie from'../src/assets/barbie.png'
 import { createContext } from "react";
+import {FaReact,FaPython,FaNodeJs} from 'react-icons/fa'
+import {BiLogoFlask,BiLogoPostgresql} from 'react-icons/bi'
 
 export const contextoPrincipal = createContext();
 
 export function ContextoProveedor({children}){
+    const logosSobremiSize=130;
+    const colorSkills='rgba(255, 255, 255, 0.74)';
     const proyectos = [
         {
             'titulo':'App Puc (Plan unico de cuentas)',
             'tecnologias':['React Native'],
             'img':puc,
             'git':'https://github.com/desingkuro/Puc-App.git',
-            'link':'https://puc.uptodown.com/android',
+            //'link':'https://puc.uptodown.com/android',
+            'link':'https://puc-app-mobile.netlify.app/',
             'descripcion':'App móvil desarrollada en React Native para realizar búsquedas de cuentas o códigos de contabilidad.'
         },
         {
@@ -50,8 +55,105 @@ export function ContextoProveedor({children}){
             'descripcion':'Landing Page basada en la página oficial de Tesla y midudev.'
         },
     ]
+    const skills=[
+        {
+            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'React'
+        },
+        {
+            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'React Native'
+        },
+        {
+            'item':<FaPython size={logosSobremiSize} color={colorSkills}/>  ,
+            'nombre':'Python'
+        },
+        {
+            'item':<BiLogoFlask size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'Flask'
+        },
+        {
+            'item':<BiLogoPostgresql size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'PostGres'
+        },
+        {
+            'item':<FaNodeJs size={logosSobremiSize} color={colorSkills}/> ,
+            'nombre':'Node Js'
+        },
+        {
+            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'React'
+        },
+        {
+            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'React Native'
+        },
+        {
+            'item':<FaPython size={logosSobremiSize} color={colorSkills}/>  ,
+            'nombre':'Python'
+        },
+        {
+            'item':<BiLogoFlask size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'Flask'
+        },
+        {
+            'item':<BiLogoPostgresql size={logosSobremiSize} color={colorSkills}/>,
+            'nombre':'PostGres'
+        },
+        {
+            'item':<FaNodeJs size={logosSobremiSize} color={colorSkills}/> ,
+            'nombre':'Node Js'
+        }
+    ]
+    const data = [
+        {
+          type: "Formaciones",
+          items: [
+            {
+              title: "Ingenieria de Sistemas",
+              institution: "Universidad del Magdalena",
+              date: "Febrero 2020 – Presente"
+            }
+          ]
+        },
+        {
+          type: "Cursos",
+          items: [
+            {
+              title: "Programación Basica",
+              institution: "Platzi",
+              date: "Noviembre 2019 – Noviembre 2019"
+            },
+            {
+              title: "React: desarrollando con JavaScript",
+              institution: "Alura Latam",
+              date: "Junio 2023 – Junio 2023"
+            },
+            {
+              title: "JS en la Web: Manipulación del DOM con JavaScript",
+              institution: "Alura Latam",
+              date: "Mayo 2023 – Mayo 2023"
+            },
+            {
+                title: "React Router: Navegación en una SPA",
+                institution: "Alura Latam",
+                date: "Mayo 2023 – Mayo 2023"
+            },
+            {
+              title: "Git y GitHub: controle y comparta su código",
+              institution: "Alura Latam",
+              date: "Abril 2023 – Abril 2023"
+            },
+            {
+                title: "Gestión ágil: Liderando el cambio en un ambiente de agilidad",
+                institution: "Alura Latam",
+                date: "Abril 2023 – Abril 2023"
+            },
+          ]
+        }
+      ];
     return(
-        <contextoPrincipal.Provider value={{proyectos}}>
+        <contextoPrincipal.Provider value={{proyectos,skills,data}}>
             {children}
         </contextoPrincipal.Provider>
     )
--- src/pages/Sobremi.jsx
+++ src/pages/Sobremi.jsx
@@ -1,117 +1,57 @@
-import '../style/SobremiStyle.css'
-import logo from '../assets/logoKurowhite.png'
-import {FaReact,FaPython,FaNodeJs} from 'react-icons/fa'
-import {BiLogoFlask,BiLogoPostgresql} from 'react-icons/bi'
-import { ItemSkills } from '../components/ItemSkill'
+import "../style/SobremiStyle.css";
+import logo from "../assets/logoKurowhite.png";
+import { FaReact, FaPython, FaNodeJs } from "react-icons/fa";
+import { BiLogoFlask, BiLogoPostgresql } from "react-icons/bi";
+import { ItemSkills } from "../components/ItemSkill";
+import { useContext } from "react";
+import { contextoPrincipal } from "../../context/contexto";
 
-export const Sobremi=()=>{
-    const logosSobremiSize=130;
-    const colorSkills='rgba(255, 255, 255, 0.74)';
-    const skills=[
-        {
-            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'React'
-        },
-        {
-            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'React Native'
-        },
-        {
-            'item':<FaPython size={logosSobremiSize} color={colorSkills}/>  ,
-            'nombre':'Python'
-        },
-        {
-            'item':<BiLogoFlask size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'Flask'
-        },
-        {
-            'item':<BiLogoPostgresql size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'PostGres'
-        },
-        {
-            'item':<FaNodeJs size={logosSobremiSize} color={colorSkills}/> ,
-            'nombre':'Node Js'
-        },
-        {
-            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'React'
-        },
-        {
-            'item':<FaReact size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'React Native'
-        },
-        {
-            'item':<FaPython size={logosSobremiSize} color={colorSkills}/>  ,
-            'nombre':'Python'
-        },
-        {
-            'item':<BiLogoFlask size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'Flask'
-        },
-        {
-            'item':<BiLogoPostgresql size={logosSobremiSize} color={colorSkills}/>,
-            'nombre':'PostGres'
-        },
-        {
-            'item':<FaNodeJs size={logosSobremiSize} color={colorSkills}/> ,
-            'nombre':'Node Js'
-        }
-    ]
-    return(
-        <section className="sobremiContainer">
-            <main className='contenidoPrincipal'>
-                <section className="sectionSobremi">
-                    {skills.map((e,i)=>{
-                        return(
-                            <ItemSkills key={i} item={e}/>
-                        )
-                    })}
-                </section>
-                <section className="sectionSobremi">
-                    <img src={logo} alt="Logotipo de Kuro" className='logoSobremi'/>
-                    <h2 className='tituloSobremi'>Mas sobre mi</h2>
-                    <p>
-                        Soy un desarrollador Front-End, enfocado en el diseño estético, 
-                        la usabilidad y el rendimiento. También cuento con buenas habilidades de comunicación,
-                        edición y diseño, así como conocimientos de diferentes lenguajes. Si buscas una mente creativa y hábil en desarrollo 
-                        frontend para tu próximo proyecto, estás en el lugar correcto.
-                    </p>
-                </section>
-                <section className="sectionSobremi">
-                    <div className='estudios'>
-                        <section className='formaciones'>
-                            <h2>Formaciones</h2>
-                            <ul>
-                                <li>
-                                    <h3>Ingenieria de Sistemas,<span className='resaltadoFormaciones'> Universidad del Magdalena</span></h3>
-                                    <span>Febrero 2020 – Presente</span>
-                                </li>
-                            </ul>
-                        </section>
-                        <section className='formaciones'>
-                            <h2>Cursos</h2>
-                            <ul>
-                                <li>
-                                    <h3>Programación Basica, <span className='resaltadoFormaciones'>Platzi</span></h3>
-                                    <span>Noviembre 2019 – Noviembre 2019</span>
-                                </li>
-                                <li>
-                                    <h3>React: desarrollando con JavaScript, <span className='resaltadoFormaciones'>Alura Latam</span></h3>
-                                    <span>Junio 2023 – Junio 2023</span>
-                                </li>
-                                <li>
-                                    <h3>JS en la Web: Manipulación del DOM con JavaS cript,<span className='resaltadoFormaciones'>Alura Latam</span></h3>
-                                    <span>Mayo 2023 – Mayo 2023</span>
-                                </li>
-                                <li>
-                                    <h3>Git y GitHub: controle y comparta su código,<span className='resaltadoFormaciones'>Alura Latam</span></h3>
-                                    <span>Abril 2023 – Abril 2023</span>
-                                </li>
-                            </ul>
-                        </section>
-                    </div>
-                </section>
-            </main>
+export const Sobremi = () => {
+  const { skills, data } = useContext(contextoPrincipal);
+
+  return (
+    <section className="sobremiContainer">
+      <main className="contenidoPrincipal">
+        <section className="sectionSobremi">
+          {skills.map((e, i) => {
+            return <ItemSkills key={i} item={e} />;
+          })}
         </section>
-    )
-}
+        <section className="sectionSobremi">
+          <img src={logo} alt="Logotipo de Kuro" className="logoSobremi" />
+          <h2 className="tituloSobremi">Mas sobre mi</h2>
+          <p>
+            Soy un desarrollador Front-End, enfocado en el diseño estético, la
+            usabilidad y el rendimiento. También cuento con buenas habilidades
+            de comunicación, edición y diseño, así como conocimientos de
+            diferentes lenguajes. Si buscas una mente creativa y hábil en
+            desarrollo frontend para tu próximo proyecto, estás en el lugar
+            correcto.
+          </p>
+        </section>
+        <section className="sectionSobremi">
+          <div className="estudios">
+            {data.map((section, index) => (
+              <section className="formaciones" key={index}>
+                <h2>{section.type}</h2>
+                <ul>
+                  {section.items.map((item, index) => (
+                    <li key={index}>
+                      <h3>
+                        {item.title},{" "}
+                        <span className="resaltadoFormaciones">
+                          {item.institution}
+                        </span>
+                      </h3>
+                      <span>{item.date}</span>
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            ))}
+          </div>
+        </section>
+      </main>
+    </section>
+  );
+};
